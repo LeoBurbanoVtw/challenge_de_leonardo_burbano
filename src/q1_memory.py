fa0d5820f5512d4c6f22a1a1dc6eca686495f28e
Leonardo Burbano <leonardo.burbano@thoughtworks.com>
2024-05-02 10:11:46 -0500

    Finished q1: basic implementation

--- src/q1_memory.py
+++ src/q1_memory.py
@@ -6,5 +6,5 @@ from utils.queries import q1
 def q1_memory(file_path: str) -> List[Tuple[datetime.date, str]]:
     bigquery_client = BigQueryClient()
     result = bigquery_client.read_table(sql_query=q1)
-    list_output = [(row[1], row[0]) for row in result]
+    list_output = [(row[0], row[1]) for row in result]
     return list_output
