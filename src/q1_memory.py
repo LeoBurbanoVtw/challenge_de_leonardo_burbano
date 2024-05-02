0a6fed625006e9b71853800fefc3328d582c5356
Leonardo Burbano <leonardo.burbano@thoughtworks.com>
2024-05-02 11:25:30 -0500

    Implementing time and memory measure methods

--- src/q1_memory.py
+++ src/q1_memory.py
@@ -2,7 +2,9 @@ from typing import List, Tuple
 from datetime import datetime
 from db.bigquery import BigQueryClient
 from utils.queries import q1
+from memory_profiler import profile
 
+@profile
 def q1_memory(file_path: str) -> List[Tuple[datetime.date, str]]:
     bigquery_client = BigQueryClient()
     result = bigquery_client.read_table(sql_query=q1)
