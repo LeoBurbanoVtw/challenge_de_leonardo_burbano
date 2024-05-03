06e8131d6bb3b17f94f79b5c6ee64480e4db18e4
Leonardo Burbano <leonardo.burbano@thoughtworks.com>
2024-05-02 23:06:34 -0500

    Finshed gdrive_to_cloud_storage, improved pipeline

--- src/db/cloudstorage.py
+++ src/db/cloudstorage.py
@@ -1,5 +1,8 @@
 from google.cloud import storage
 from google.cloud.exceptions import NotFound
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class CloudStorageClient:
     """
@@ -41,4 +44,35 @@ class CloudStorageClient:
         except NotFound:
             # Handle the case where the bucket does not exist
             print(f"Bucket {bucket_name} not found.")
+            return None
+        
+    def upload_to_gcs(self, file_path, bucket_name, destination_blob_name):
+        """
+        Uploads a file from a specified path to Google Cloud Storage (GCS) bucket.
+
+        Args:
+            file_path (str): The path to the file to upload.
+            bucket_name (str): The name of the GCS bucket where the file will be uploaded.
+            destination_blob_name (str): The name of the blob (object) in the bucket.
+
+        Returns:
+            str or None: The GCS URI of the uploaded file if successful,
+                        or None if an error occurs during upload.
+        """
+        try:
+            # Read file contents
+            with open(file_path, "rb") as file:
+                file_contents = file.read()
+
+            # Upload the file contents to Google Cloud Storage
+            bucket = self.client.bucket(bucket_name)
+            blob = bucket.blob(destination_blob_name)
+            blob.upload_from_string(file_contents)
+
+            # Return the GCS URI of the uploaded file
+            return f"gs://{bucket_name}/{destination_blob_name}"
+
+        except Exception as e:
+            # If an error occurs during upload
+            print(f"Error uploading to GCS: {e}")
             return None
